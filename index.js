5f12dfbdbeba1eacf9f8382b6cab7f6ed632c932
psawn <psawn0972055909@yahoo.com.vn>
2020-09-16 19:35:36 +0700

    giao dien home

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// tao may chu
+app.listen(PORT,function () {
+    console.log("server is running...");
+})
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+// tao cau hinh ket noi Sql server
+const mssql = require("mssql");
+const config = {
+    server: '101.99.13.2',
+    user:'sa',
+    password:'z@GH7ytQ',
+    database:'test'
+}
+// connect db
+mssql.connect(config,function (err) {
+    if(err) console.log(err);
+    else console.log("ket noi DB thanh cong!");
+})
+// tao doi tuong truy van
+const db = new mssql.Request();
+
+app.get("/",function (req,res) {
+    res.render("home");
+})
