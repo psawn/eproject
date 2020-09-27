f1b1a44105877deea4150d32ca38a8ea7527411d
Reborn92 <65600809+Reborn92@users.noreply.github.com>
2020-09-27 23:09:13 +0700

    Update about us and liveshow page

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 
 // tao cau hinh ket noi Sql server
-const mssql = require("mssql");
-const config = {
-    server: '101.99.13.2',
-    user:'sa',
-    password:'z@GH7ytQ',
-    database:'test'
-}
+// const mssql = require("mssql");
+// const config = {
+//     server: '101.99.13.2',
+//     user:'sa',
+//     password:'z@GH7ytQ',
+//     database:'test'
+// }
 // connect db
-mssql.connect(config,function (err) {
-    if(err) console.log(err);
-    else console.log("ket noi DB thanh cong!");
-})
-// tao doi tuong truy van
-const db = new mssql.Request();
+// mssql.connect(config,function (err) {
+//     if(err) console.log(err);
+//     else console.log("ket noi DB thanh cong!");
+// // })
+// // tao doi tuong truy van
+// const db = new mssql.Request();
 
 app.get("/",function (req,res) {
     res.render("home");
@@ -34,4 +34,10 @@ app.get("/dang-ky",function (req,res) {
 })
 app.get("/dang-nhap",function (req,res) {
     res.render("dangnhap");
+})
+app.get("/about-us",function (req,res){
+    res.render("aboutus")
+})
+app.get("/live-show",function (req,res){
+    res.render("liveshow")
 })
